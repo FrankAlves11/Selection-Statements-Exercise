bf60539c062e6764a61ac9a1668e205d98c34589
FrankAlves11 <Frankie.alves@icloud.com>
2023-08-29 09:43:33 -0700

    made changes

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Can you guess my favorite number between 1 and 11?");
+            var userInput = int.Parse(Console.ReadLine());
+            int favoriteNumber = 11;
+
+            if (favoriteNumber < userInput)
+            {
+                Console.WriteLine("Your guess was too high!");
+            }
+
+            else if (favoriteNumber > userInput)
+            {
+                Console.WriteLine("Your number was too low!");
+            }
+
+            else
+            {
+                Console.WriteLine("Congratulations you are victorious!");
+            }
+
+
+
         }
     }
-}
+}    
